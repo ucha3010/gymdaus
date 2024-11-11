7ebc27703e1f37347f926a12605cc513a247ac0c
Damian <damianjava@gmail.com>
2024-11-11 20:12:10 +0100

    Logo empresa y foto perfil en barra usuario

--- src/main/java/com/gymdaus/core/service/impl/UtilServiceImpl.java
+++ src/main/java/com/gymdaus/core/service/impl/UtilServiceImpl.java
@@ -3,6 +3,7 @@ package com.gymdaus.core.service.impl;
 import com.gymdaus.core.model.UserModel;
 import com.gymdaus.core.service.CountryService;
 import com.gymdaus.core.service.Menu1Service;
+import com.gymdaus.core.service.UserDocumentManagerService;
 import com.gymdaus.core.service.UtilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,8 @@ public class UtilServiceImpl implements UtilService {
     @Autowired
     private Menu1Service menu1Service;
     @Autowired
+    private UserDocumentManagerService userDocumentManagerService;
+    @Autowired
     private UserService userService;
 
     @Override
@@ -34,6 +37,7 @@ public class UtilServiceImpl implements UtilService {
             UserModel user = userService.getLoggedUserModel();
             user.setPassword(null);
             modelAndView.addObject("user", user);
+            modelAndView.addObject("profilePhoto", userDocumentManagerService.getProfilePhotoPath(user.getUsername()));
             modelAndView.addObject("menu1List", menu1Service.findAllEnabled());
             modelAndView.addObject("menu1GymList", menu1Service.findMenu1GymList());
             return user;
